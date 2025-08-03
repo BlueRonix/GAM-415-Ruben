7032be1f5af2e718f024ce234ac037f41020a757
BlueRonix <ruben.garcia6@snhu.edu>
2025-08-03 15:08:51 -0500

    Scene Capture Component & Render Target
    An object reflects the arena which can be messed with in the context of the position

--- firstperson415/Intermediate/Build/BuildRulesProjects/firstperson415ModuleRules/obj/Development/firstperson415ModuleRules.AssemblyInfo.cs
+++ firstperson415/Intermediate/Build/BuildRulesProjects/firstperson415ModuleRules/obj/Development/firstperson415ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstperson415ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f281e5346741243757c8e6656af235beb80a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a713c9d4386978447c59a295f0cbaf2493630fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstperson415ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstperson415ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
